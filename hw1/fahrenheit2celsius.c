2abc2e707561f3e5d5d27249eb9abe6ad321677e
ricardo sida <rsida2@miners.utep.edu>
2021-09-05 23:39:40 -0600

    commnets

--- hw1/fahrenheit2celsius.c
+++ hw1/fahrenheit2celsius.c
@@ -15,11 +15,13 @@ int main() {
 	  fahr = lower;
 	  while (fahr <= upper) {
           celsius = convert(fahr);
+		  /* to print we use %.4f saying we only want four decimals of the float variable*/
           printf("%d\t%.4f\n",fahr, celsius);
 		  fahr = fahr + step;
 	  }
 }
 float convert (float f){
+	/* using the formula for conversion*/
           return 5 * (f-32) / 9;
           
 }
