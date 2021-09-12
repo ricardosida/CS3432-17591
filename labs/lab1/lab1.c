553846a65a535b9c32d28bf7e7fe5e20db7730ac
ricardo sida <rsida2@miners.utep.edu>
2021-09-12 16:07:14 -0600

    added changes

--- labs/lab1/lab1.c
+++ labs/lab1/lab1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#define MAX_LIMIT 20
+char** tokenize(char* str);
 
 int main()
 {
+    char str[MAX_LIMIT];
+    fgets(str, MAX_LIMIT, stdin);
 
+    char* x = str;
+    tokenize(x);
+}
+
+char** tokenize(char* str) {
+    char* new_string = (char*) malloc(str_size * sizeof(str));
+    printf("%s", new_string);
+    return 0;
 }
